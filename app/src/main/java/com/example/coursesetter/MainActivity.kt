9e016c90965e995ac4867a8666fff089a905ff22
ndstancilFullsail <ndstancil@student.fullsail.edu>
2024-02-17 15:42:56 -0500

    added side nav functions and xml files

--- app/src/main/java/com/example/coursesetter/MainActivity.kt
+++ app/src/main/java/com/example/coursesetter/MainActivity.kt
@@ -8,33 +8,55 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.coursesetter.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.core.view.GravityCompat
 
 
+class MainActivity : AppCompatActivity() {
 
-    class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
-        private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-            binding = ActivityMainBinding.inflate(layoutInflater)
-            setContentView(binding.root)
+        val navView: BottomNavigationView = binding.navView
 
-            val navView: BottomNavigationView = binding.navView
-
-            val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
             // Passing each menu ID as a set of Ids because each
             // menu should be considered as top level destinations.
-            val appBarConfiguration = AppBarConfiguration(
-                setOf(
-                    R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-                )
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
             )
-            setupActionBarWithNavController(navController, appBarConfiguration)
-            navView.setupWithNavController(navController)
+        )
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        val sideNavView: NavigationView = binding.navViewSide
+
+
+        sideNavView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.nav_stats -> {
+                    navController.navigate(R.id.statsFragment)
+                    drawerLayout.closeDrawer(GravityCompat.START)
+
+                    true
+                }
+
+                else -> false
+
+            }
         }
     }
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
+
 
 
 
